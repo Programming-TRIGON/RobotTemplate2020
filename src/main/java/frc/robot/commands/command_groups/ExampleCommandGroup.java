1110422b7c2fe5c23c2ce08744dd1f9cd0072c10
Hillel <Amiel>
2020-01-10 15:21:20 +0200

    added commands folder
    with command group folder in it

---
+++ src/main/java/frc/robot/commands/command_groups/ExampleCommandGroup.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.command_groups;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+/**
+ * Command groups without should be in this folder.
+ * For example, this parallel command group.
+ */
+public class ExampleCommandGroup extends ParallelCommandGroup {
+  /**
+   * Creates a new ExampleCommandGroup.
+   */
+  public ExampleCommandGroup() {
+  }
+}
