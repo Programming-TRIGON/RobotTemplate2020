d06e522fd61822cf2f33d4764fccc0ddd7dc8104
Hillel <Amiel>
2020-01-12 16:11:02 +0200

    CR commant update
    in Command groups

--- src/main/java/frc/robot/commands/command_groups/ExampleCommandGroup.java
+++ src/main/java/frc/robot/commands/command_groups/ExampleCommandGroup.java
@@ -3,7 +3,8 @@ package frc.robot.commands.command_groups;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 /**
- * Command groups without should be in this folder.
+ * Command groups should be in this folder.
+ * These commands will probably be used for Driver and Operator control (OI).
  * For example, this parallel command group.
  */
 public class ExampleCommandGroup extends ParallelCommandGroup {
