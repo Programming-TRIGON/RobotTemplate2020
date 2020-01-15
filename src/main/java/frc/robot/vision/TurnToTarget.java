0dc2ed07090f2a3bde610634920e46af16984cda
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2020-01-15 10:53:16 +0200

    remove output variable from TurnToTarget

--- src/main/java/frc/robot/vision/TurnToTarget.java
+++ src/main/java/frc/robot/vision/TurnToTarget.java
@@ -4,10 +4,8 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.vision.Target;
 import frc.robot.subsystems.MoveableSubsystem;
 import frc.robot.utils.PIDSettings;
-import java.util.function.DoubleConsumer;
 
 import static frc.robot.Robot.limelight;
 import static frc.robot.Robot.robotConstants;
@@ -17,20 +15,19 @@ import static frc.robot.Robot.robotConstants;
  * It will be probably changed according the game and the robot.
  */
 public class TurnToTarget extends CommandBase {
+  private final MoveableSubsystem subsystem;
   private Target target;
-  private DoubleConsumer output;
   private PIDController rotationPIDController;
   private double lastTimeSeenTarget;
 
   /**
    * @param target    The target the robot will follow
-   * @param output    accepts rotation output.
    * @param subsystem the subsystem to require
    */
   public TurnToTarget(Target target, MoveableSubsystem subsystem) {
     addRequirements(subsystem);
     this.target = target;
-    output = subsystem::move;
+    this.subsystem = subsystem;
     PIDSettings rotationSettings = robotConstants.controlConstants.visionRotationSettings;
     rotationPIDController = new PIDController(rotationSettings.getKP(), rotationSettings.getKI(), rotationSettings.getKD());
     rotationPIDController.setTolerance(rotationSettings.getTolerance(), rotationSettings.getDeltaTolerance());
@@ -48,11 +45,11 @@ public class TurnToTarget extends CommandBase {
   @Override
   public void execute() {
     if (limelight.getTv()) {
-      output.accept(rotationPIDController.calculate(limelight.getAngle()));
+      subsystem.move(rotationPIDController.calculate(limelight.getAngle()));
       lastTimeSeenTarget = Timer.getFPGATimestamp();
     } else
       // The target wasn't found
-      output.accept(0.0);
+      subsystem.stopMove();
   }
 
   @Override
@@ -63,7 +60,7 @@ public class TurnToTarget extends CommandBase {
 
   @Override
   public void end(boolean interrupted) {
-    output.accept(0.0);
+    subsystem.stopMove();
     limelight.stopVision();
   }
 
